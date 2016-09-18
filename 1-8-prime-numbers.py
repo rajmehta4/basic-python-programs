1f57d912636365ba8ac70866b0b3a3457b964744
Raj Mehta <rraajjmehta@gmail.com>
2016-09-18 23:42:17 +0530

    4 new solutions added :)

---
+++ 1-8-prime-numbers.py
@@ -0,0 +1,14 @@
+n = int(input("Let's just stick to the tradition: enter the value of n please!\n"))
+
+print("The list of prime numbers from 2 to", n, "is:")
+for num in range(2, n+1):
+	flag = False
+	iterator = num - 1
+	while(iterator > 1):
+		if(num%iterator == 0):	#The number is divisible by some other number! Not prime! 
+			flag = True
+			break				#no point in continuing now that we know the given number is not prime
+		iterator -= 1
+
+	if(flag == False):			#if true, it means that the given number wasn't divisible by any other number. Hence, prime!
+		print(num)
