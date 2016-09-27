9875a529260b53e1f38108effda29294b0acf17a
Raj Mehta <rraajjmehta@gmail.com>
2016-09-27 19:04:17 +0530

    improved logic for prime numbers program

--- 1-8-prime-numbers.py
+++ 1-8-prime-numbers.py
@@ -1,14 +1,15 @@
 n = int(input("Let's just stick to the tradition: enter the value of n please!\n"))
 
-print("The list of prime numbers from 2 to", n, "is:")
-for num in range(2, n+1):
-	flag = False
-	iterator = num - 1
-	while(iterator > 1):
-		if(num%iterator == 0):	#The number is divisible by some other number! Not prime! 
-			flag = True
-			break				#no point in continuing now, that we know the given number is not prime
-		iterator -= 1
+print("The list of prime numbers from 2 to {} is:".format(n))
 
-	if(flag == False):			#if true, it means that the given number wasn't divisible by any other number. Hence, prime!
+for num in range(2, n+1):
+
+	flag = False
+
+	for iterator in range(2, num):
+		if(num % iterator == 0):
+			flag = True
+			break
+
+	if(flag == False):
 		print(num)
